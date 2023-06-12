4f787f49497ac6d02765fbc780900009f1095e4d
qiushaocloud <qiushaocloud@gmail.com>
2023-06-12 19:36:19 +0800

    update

--- k8s-deploy/hostpath-pv-pvc/init-all-pv-pvc.sh
+++ k8s-deploy/hostpath-pv-pvc/init-all-pv-pvc.sh
@@ -7,7 +7,7 @@ set +a
 for port in `seq 6373 6378`; do
   REDIS_PORT_NUMBER=${port}
   cp -ra redis-cluster-node.pv-pvc.REDIS_PORT_NUMBER.yaml.tpl redis-cluster-node.pv-pvc.${REDIS_PORT_NUMBER}.yaml
-  sed -i s/\<REDIS_PORT_NUMBER\>/${REDIS_PORT_NUMBER}/g redis-cluster-node.pv-pvc.${REDIS_PORT_NUMBER}.yaml
-  sed -i s/\<LOCAL_STORAGE_PATH\>/${LOCAL_STORAGE_PATH}/g redis-cluster-node.pv-pvc.${REDIS_PORT_NUMBER}.yaml
-  sed -i s/\<K8S_PV_STORAGE_SIZE\>/${K8S_PV_STORAGE_SIZE}/g redis-cluster-node.pv-pvc.${REDIS_PORT_NUMBER}.yaml
+  sed -i "s#<REDIS_PORT_NUMBER>#${REDIS_PORT_NUMBER}#g" redis-cluster-node.pv-pvc.${REDIS_PORT_NUMBER}.yaml
+  sed -i "s#<LOCAL_STORAGE_PATH>#${LOCAL_STORAGE_PATH}#g" redis-cluster-node.pv-pvc.${REDIS_PORT_NUMBER}.yaml
+  sed -i "s#<K8S_PV_STORAGE_SIZE>#${K8S_PV_STORAGE_SIZE}#g" redis-cluster-node.pv-pvc.${REDIS_PORT_NUMBER}.yaml
 done
