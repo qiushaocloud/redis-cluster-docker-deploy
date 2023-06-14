35f320013ad757f67353f9a9b30963578bc59dd7
qiushaocloud <qiushaocloud@126.com>
2023-06-15 01:27:01 +0800

    update push sh file

--- push-remote-git-repos.sh
+++ push-remote-git-repos.sh
@@ -2,10 +2,6 @@ CURR_DIR=$(cd "$(dirname "$0")"; pwd)
 
 gitName=`grep -E "url = .*.git" $CURR_DIR/.git/config|awk -F 'qiushaocloud/' '{print $2}'`
 
-# 推送到自建的 gitlab
-git remote set-url origin ssh://git@www.qiushaocloud.top:61023/qiushaocloud/$gitName
-git push
-
 # 推送到 gitee
 git remote set-url origin git@gitee.com:qiushaocloud/$gitName
 git push
@@ -14,3 +10,10 @@ git push
 git remote set-url origin git@github.com:qiushaocloud/$gitName
 git push
 
+# 推送到自建的 gitea
+git remote set-url origin ssh://git@gitee.qiushaocloud.top:61322/qiushaocloud/$gitName
+git push
+
+# 推送到自建的 gitlab
+git remote set-url origin ssh://git@gitlab.qiushaocloud.top:61023/qiushaocloud/$gitName
+git push
