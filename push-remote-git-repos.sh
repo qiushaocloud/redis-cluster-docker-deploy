20e15daaa627d2374794fc897c6ab7929008dd52
qiushaocloud <qiushaocloud@126.com>
2023-06-18 10:15:07 +0800

    update sh

--- push-remote-git-repos.sh
+++ push-remote-git-repos.sh
@@ -13,25 +13,14 @@ git remote set-url origin git@github.com:qiushaocloud/$gitName
 git push
 
 # 推送到 gitlab
-echo "推送到 gitlab: git@gitlab.com:qiushaocloud/$gitName"
-git remote set-url origin git@gitlab.com:qiushaocloud/$gitName
+echo "推送到 gitlab: git@gitlab.com:qiushaoyumeng/$gitName"
+git remote set-url origin git@gitlab.com:qiushaoyumeng/$gitName
 git push
 
 # 推送到 gitcode
 echo "推送到 gitcode: git@gitcode.net:qiushaocloud/$gitName"
 git remote set-url origin git@gitcode.net:qiushaocloud/$gitName
 git push
-
-# 推送到 gitea
-echo "推送到 gitea: git@gitea.com:qiushaocloud/$gitName"
-git remote set-url origin git@gitea.com:qiushaocloud/$gitName
-git push
-
-# 推送到 gogs
-echo "推送到 gogs: git@try.gogs.io:qiushaocloud/$gitName"
-git remote set-url origin git@try.gogs.io:qiushaocloud/$gitName
-git push
-
 
 # 推送到自建的 gitea
 echo "推送到自建的 gitea: ssh://git@gitea.qiushaocloud.top:61322/qiushaocloud/$gitName"
